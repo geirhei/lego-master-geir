50c1b771e3764648a142468b6c2f06a2e9d8a30b
geirhei <geirhei@stud.ntnu.no>
2017-10-07 13:08:03 +0200

    motor direction defines

--- NXT/Includes/motor.c
+++ NXT/Includes/motor.c
@@ -21,117 +21,25 @@ void vMotor_init(void) {
   nxt_motor_set_count(servoRight, 0);
   nxt_motor_set_count(servoTower, 0);
 }
-/* Switch for robot movement to abstract the logic away from main */
-/* Comparing ticks ensures balanced movement */
-/*
-void vMotorMovementSwitch(uint8_t movement, int16_t tmp_leftWheelTicks, int16_t tmp_rightWheelTicks){
-  switch (movement){
-	case moveForward:{
-	  if(tmp_leftWheelTicks > tmp_rightWheelTicks) {
-		nxt_motor_set_speed(servoLeft, SPEED-10, 1);
-		nxt_motor_set_speed(servoRight, SPEED, 1);
-	  } else if(tmp_rightWheelTicks > tmp_leftWheelTicks) {
-		nxt_motor_set_speed(servoLeft, SPEED, 1);
-		nxt_motor_set_speed(servoRight, SPEED-10, 1);
-	  } else {
-		nxt_motor_set_speed(servoLeft, SPEED, 1);
-		nxt_motor_set_speed(servoRight, SPEED, 1);
-	  }
-	  break;
-	}
-	case moveBackward:{
-	  if(tmp_leftWheelTicks < tmp_rightWheelTicks) {
-		nxt_motor_set_speed(servoLeft, -SPEED+10, 1);
-		nxt_motor_set_speed(servoRight, -SPEED, 1);
-	  } else if(tmp_rightWheelTicks < tmp_leftWheelTicks) {
-		nxt_motor_set_speed(servoLeft, -SPEED, 1);
-		nxt_motor_set_speed(servoRight, -SPEED+10, 1);
-	  } else {
-		nxt_motor_set_speed(servoLeft, -SPEED, 1);
-		nxt_motor_set_speed(servoRight, -SPEED, 1);
-	  }
-	  
-	  break;
-	}
-	case moveClockwise:{
-	  if(tmp_leftWheelTicks > -tmp_rightWheelTicks) {
-		nxt_motor_set_speed(servoLeft, SPEED-10, 1);
-		nxt_motor_set_speed(servoRight, -SPEED, 1);
-	  } else if(-tmp_rightWheelTicks > tmp_leftWheelTicks) {
-		nxt_motor_set_speed(servoLeft, SPEED, 1);
-		nxt_motor_set_speed(servoRight, -SPEED+10, 1);
-	  } else {
-		nxt_motor_set_speed(servoLeft, SPEED, 1);
-		nxt_motor_set_speed(servoRight, -SPEED, 1);
-	  }
-	  
-	  break;
-	}
-	case moveRightForward:{
-	  nxt_motor_set_speed(servoRight, SPEED, 1);
-	  nxt_motor_set_speed(servoLeft, 0, 1);
-	  break;
-	}
-	case moveLeftForward:{
-	  nxt_motor_set_speed(servoLeft, SPEED, 1);
-	  nxt_motor_set_speed(servoRight, 0, 1);
-	  break;
-	}
-	case moveRightBackward:{
-	  nxt_motor_set_speed(servoRight, -SPEED, 1);
-	  nxt_motor_set_speed(servoLeft, 0, 1);
-	  break;
-	}
-	case moveLeftBackward:{
-	  nxt_motor_set_speed(servoLeft, -SPEED, 1);
-	  nxt_motor_set_speed(servoRight, 0, 1);
-	  break;
-	}
-	case moveCounterClockwise:{
-	  if(-tmp_leftWheelTicks > tmp_rightWheelTicks) {
-		nxt_motor_set_speed(servoLeft, -SPEED+10, 1);
-		nxt_motor_set_speed(servoRight, SPEED, 1);
-	  } else if(tmp_rightWheelTicks > -tmp_leftWheelTicks) {
-		nxt_motor_set_speed(servoLeft, -SPEED, 1);
-		nxt_motor_set_speed(servoRight, SPEED-10, 1);
-	  } else {
-		nxt_motor_set_speed(servoLeft, -SPEED, 1);
-		nxt_motor_set_speed(servoRight, SPEED, 1);
-	  }
-	  break;
-	}
-	default:{
-	  nxt_motor_set_speed(servoLeft, 0, 1);
-	  nxt_motor_set_speed(servoRight, 0, 1);
-	  break;
-	}
-  }
-}
-*/
 
-// New functions
 static void vMotorMoveLeftForward(uint8_t actuation, uint8_t *leftWheelDirection) {
     nxt_motor_set_speed(servoLeft, actuation, 1);
-    nxt_motor_set_speed(servoRight, 0, 1);
-    *leftWheelDirection = motorLeftForward;
+    *leftWheelDirection = motorForward;
 }
 
 static void vMotorMoveRightForward(uint8_t actuation, uint8_t *rightWheelDirection) {
 	nxt_motor_set_speed(servoRight, actuation, 1);
-	nxt_motor_set_speed(servoLeft, 0, 1);
-	*rightWheelDirection = motorRightForward;
+	*rightWheelDirection = motorForward;
 }
 
 static void vMotorMoveLeftBackward(uint8_t actuation, uint8_t *leftWheelDirection) {
 	nxt_motor_set_speed(servoLeft, -actuation, 1);
-    nxt_motor_set_speed(servoRight, 0, 1);
-    *leftWheelDirection = motorLeftBackward;
+    *leftWheelDirection = motorBackward;
 }
 
 static void vMotorMoveRightBackward(uint8_t actuation, uint8_t *rightWheelDirection) {
 	nxt_motor_set_speed(servoRight, -actuation, 1);
-    nxt_motor_set_speed(servoLeft, 0, 1);
-    *rightWheelDirection = motorRightBackward;
+    *rightWheelDirection = motorBackward;
 }
 
 void vMotorBrakeLeft() {
