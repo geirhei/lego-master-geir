56b5edf2effc52cbba458fb84deb425d44bd7591
geirhei <geirhei@stud.ntnu.no>
2018-01-23 15:53:49 +0100

    comment added

--- NXT/Includes/communication.h
+++ NXT/Includes/communication.h
@@ -24,6 +24,10 @@ extern QueueHandle_t sendingQ;
  */
 void vMainCommunicationTask( void *pvParameters );
 
+/**
+ * @brief      Task that reads message from the sendingQ and sends them to the server.
+ * 				Will block if the queue is empty.
+ */
 void vSenderTask( void *pvParameters );
 
 #endif
